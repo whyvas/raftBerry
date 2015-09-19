36a530d60fd619e788d73fd0c00fb217c5da2fc5
whyvas <whyvas@gmail.com>
2015-09-19 01:29:33 -0400

    Update raftBerry.py

--- raftBerry.py
+++ raftBerry.py
@@ -76,14 +76,12 @@ def joyLeft(channel):
 	print "Left:",leftspeed," Right:",rightspeed
 	setSpeed()
 #increase right by 1, if right = 7 and left doesn't = 0, decrease left by 1
-
 def joyRight(channel):
 	print "Joystick right"
 	incLeft()
 	print "Left:",leftspeed," Right:",rightspeed
 	setSpeed()
 #increase left by 1, if left = 7 and right doesn't = 0, decrease right by 1
-
 def decLeft():
 	global leftspeed
 	if leftspeed > -3:
@@ -209,28 +207,28 @@ def turnOffset(chead,dhead):
 def read_byte(adr):
     return bus.read_byte_data(address, adr)
 def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
+	high = bus.read_byte_data(address, adr)
+	low = bus.read_byte_data(address, adr+1)
+	val = (high << 8) + low
+	return val
 def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
+	val = read_word(adr)
+	if (val >= 0x8000):
+		return -((65535 - val) + 1)
+	else:
+		return val
 def write_byte(adr, value):
-    bus.write_byte_data(address, adr, value)
+	bus.write_byte_data(address, adr, value)
 def getBearing():
 	write_byte(0, 0b01110000) # Set to 8 samples @ 15Hz
-        write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-        write_byte(2, 0b00000000) # Continuous sampling
-        scale = 0.92
-        x_out = read_word_2c(3) * scale
-        y_out = read_word_2c(7) * scale
-        z_out = read_word_2c(5) * scale
-        bearing  = math.atan2(y_out, x_out)
-        if (bearing < 0):
+	write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
+	write_byte(2, 0b00000000) # Continuous sampling
+	scale = 0.92
+	x_out = read_word_2c(3) * scale
+	y_out = read_word_2c(7) * scale
+	z_out = read_word_2c(5) * scale
+	bearing  = math.atan2(y_out, x_out)
+	if (bearing < 0):
                 bearing += 2 * math.pi
         return(math.degrees(bearing))
         
